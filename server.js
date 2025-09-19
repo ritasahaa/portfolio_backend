03d2512bb1f41538ec2adcb8d3d20074cdb0d24e
Rita Kumari Sah <ritasah1602@gmail.com>
2025-09-19 19:37:04 +0530

    fix: update CORS headers for Render deployment

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', origin);
     }
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, cache-control');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, cache-control, pragma');
     res.header('Access-Control-Allow-Credentials', 'true');
     if (req.method === 'OPTIONS') {
         res.sendStatus(200);
